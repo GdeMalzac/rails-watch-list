236f12cd8880e165e7c5b3488cd62a27a62fb2e9
Guillaume de Malzac <guillaume.de.malzac@gmail.com>
2023-02-24 13:50:51 +0100

    add dotenv-rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,8 @@ movies['results'].each do |movie|
   Movie.create(title: movie['title'], overview: movie['overview'], poster_url: movie['poster_path'], rating: movie['vote_average'])
 end
 
+puts 'seed ok'
+
 # .each do
 #   Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 # end
